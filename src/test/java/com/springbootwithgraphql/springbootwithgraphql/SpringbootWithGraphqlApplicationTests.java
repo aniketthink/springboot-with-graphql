c9fdddcd733b2ac13c5c7375c71e0cbc61f180ea
aniketthink <aniketkolate@thinkitive.com>
2023-07-10 16:10:50 +0530

    intial commit

---
+++ src/test/java/com/springbootwithgraphql/springbootwithgraphql/SpringbootWithGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootwithgraphql.springbootwithgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
